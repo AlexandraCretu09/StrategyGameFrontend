1f8045d68abacd81f564dfbb51fbae3dce47f4c8
alexa <alexabianca091@gmail.com>
2024-11-30 14:36:45 +0200

    Initial commit

---
+++ Assets/Entities/GameMap.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMap
+{
+    public int id;
+    public int mapHeight;
+    public int mapWidth;
+    public int[][] terrain;
+}
---
+++ Assets/WebSocketServerUnity.cs
@@ -0,0 +1,63 @@
+using Fleck;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebSocketServerUnity : MonoBehaviour
+{
+    private WebSocketServer server;
+    private GameMap currentGameMap;
+
+    void Start()
+    {
+        Debug.Log("Starting WebSocket server on ws://127.0.0.1:8082/terrainUpdate");
+        server = new WebSocketServer("ws://127.0.0.1:8082/terrainUpdate");
+        server.Start(socket =>
+        {
+            socket.OnOpen = () => Debug.Log("Spring connected.");
+            socket.OnClose = () => Debug.Log("Spring disconnected.");
+            socket.OnMessage = message =>
+            {
+                Debug.Log("Message received from Spring: " + message);
+                currentGameMap = JsonUtility.FromJson<GameMap>(message);
+                UpdateTerrain(currentGameMap);
+            };
+        });
+
+        Debug.Log("Unity WebSocket server started on ws://127.0.0.1:8082/terrainUpdate");
+    }
+
+    private void UpdateTerrain(GameMap gameMap)
+    {
+
+        Debug.Log("Received terrain update in Unity. Map Height: " + gameMap.mapHeight + ", Map Width: " + gameMap.mapWidth);
+
+        for (int i = 0; i < gameMap.mapHeight; i++)
+        {
+            for (int j = 0; j < gameMap.mapWidth; j++)
+            {
+                Vector3 position = new Vector3(i, 0, j);
+                if (gameMap.terrain[i][j] == 0)
+                {
+                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.position = position;
+                }
+                if (gameMap.terrain[i][j] > 0)
+                {
+                    GameObject capsule = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                    capsule.transform.position = position;
+                }
+                if (gameMap.terrain[i][j] < 0)
+                {
+                    GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+                    cylinder.transform.position = position;
+                }
+            }
+        }
+    }
+    void OnApplicationQuit()
+    {
+
+        server.Dispose();
+    }
+}
