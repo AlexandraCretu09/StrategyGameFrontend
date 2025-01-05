b7a888bce133c7e4cd81734c4894a60c44f7d1d2
alexa <alexabianca091@gmail.com>
2025-01-05 14:14:55 +0200

    multiple players can now play simultaneously on the same map

--- Assets/Scripts/WebSocketServerUnity.cs
+++ Assets/Scripts/WebSocketServerUnity.cs
@@ -10,18 +10,13 @@ public class WebSocketServerUnity : MonoBehaviour
 {
     private WebSocketServer server;
     private GameMap currentGameMap;
-    //public LobbyManager lobbyManager;
     private ConcurrentQueue<string> messageQueue = new ConcurrentQueue<string>();
 
 
     void Start()
     {
-        //Debug.Log("Starting WebSocket server on ws://127.0.0.1:8082/terrainUpdate");
         string localIP = NetworkUtils.GetLocalIPAddress();
-
         Debug.Log("Ip:   " + "ws://" + localIP + ":8082/terrainUpdate");
-
-
         server = new WebSocketServer("ws://" + localIP + ":8082/terrainUpdate");
         server.Start(socket =>
         {
@@ -114,39 +109,8 @@ public class WebSocketServerUnity : MonoBehaviour
 
     private IEnumerator RenderMapCoroutine(GameMap gameMap)
     {
-        // Wait until the next frame to ensure everything is updated before rendering
         yield return null;
 
-        // Call the render method after the frame has been processed
         LobbyManager.Instance.RenderMapFromData(gameMap);
-    }
-
-    private void UpdateTerrain(GameMap gameMap)
-    {
-
-        Debug.Log("Received terrain update in Unity. Map Height: " + gameMap.mapHeight + ", Map Width: " + gameMap.mapWidth);
-
-        for (int i = 0; i < gameMap.mapHeight; i++)
-        {
-            for (int j = 0; j < gameMap.mapWidth; j++)
-            {
-                Vector3 position = new Vector3(i, 0, j);
-                if (gameMap.terrain[i][j] == 0)
-                {
-                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    cube.transform.position = position;
-                }
-                if (gameMap.terrain[i][j] > 0)
-                {
-                    GameObject capsule = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-                    capsule.transform.position = position;
-                }
-                if (gameMap.terrain[i][j] < 0)
-                {
-                    GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-                    cylinder.transform.position = position;
-                }
-            }
-        }
     }
 }
